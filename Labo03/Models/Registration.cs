d40fefcad94a9d40b45cd7a38b3e3a3f29199875
lotterom <lotte.rombaut@student.howest.be>
2019-10-14 11:29:07 +0200

    Class Registration

---
+++ Labo03/Models/Registration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labo03.Models
+{
+    class Registration
+    {
+        public string RegistrationId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string ZipCode { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public bool IsFirstTimer { get; set; }
+    }
+}
